d806429e05754fc3b5a2cb90a9d0336f8c209405
Shadowsky66 <www.shadowsky@gmail.com>
2020-09-07 16:25:31 +0500

    исправлена ошибка проверки на наличие участников в голосовом чате

--- commands/stop.js
+++ commands/stop.js
@@ -2,6 +2,8 @@ module.exports = {
     pattern: /stop/,
     description: 'stop all',
     handler: (params, message, guildId) => {
-        message.member.voice.channel.leave();
+        if (message.member.voice.channel) {
+            message.member.voice.channel.leave();
+        }
     },
 }
